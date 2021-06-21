5dc37b4d84c1e1a292eec265f6d9f85e55972e24
unknown <adityajagdishvyawahare@gmail.com>
2021-06-21 22:54:38 +0530

    GUI and database connections added

---
+++ root.session.sql
@@ -0,0 +1,4 @@
+INSERT INTO passwords (link,answer) VALUES ('hello1','there1');
+-- SELECT * FROM passwords;
+-- DROP TABLE passwords;
+-- CREATE TABLE passwords (id INT AUTO_INCREMENT PRIMARY KEY,link VARCHAR(255), answer VARCHAR(255))
